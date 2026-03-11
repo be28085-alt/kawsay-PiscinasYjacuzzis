336e035a80e07f5d965ec4083049b280d517f103
bryan <bohorquez.brayan@correunivalle.edu.co>
2026-03-11 11:34:16 -0500

    intento 5

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import JacuzzisPage from "./pages/JacuzzisPage";
+import ServiciosPage from "./pages/ServiciosPage";
+import EquiposPage from "./pages/EquiposPage";
+import ContactoPage from "./pages/ContactoPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/jacuzzis" element={<JacuzzisPage />} />
+          <Route path="/servicios" element={<ServiciosPage />} />
+          <Route path="/equipos" element={<EquiposPage />} />
+          <Route path="/contacto" element={<ContactoPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
